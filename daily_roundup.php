deb33602eaa7102acb5c53bec7113ef01c05ff8f
KiffmeisterX <john@kiffmeister.com>
2025-08-31 22:43:41 -0400

    Update daily_roundup.php

--- daily_roundup.php
+++ daily_roundup.php
@@ -100,7 +100,7 @@ body { font-family: Arial, sans-serif; max-width: 800px; margin: 0 auto; padding
             $html .= "\n";
         }
         
-        $html .= "<hr><p><em>Curated from my bookmarks</em></p>\n";
+        $html .= "<hr><p><em>Curated from my Raindrop.io bookmarks</em></p>\n";
     }
     
     $html .= '</div>
